098ca11405c26fcd8fa221fbed616e12ac7fbd47
terry <mingyteng@163.com>
2020-09-28 15:20:35 +0800

    leetcode: populating next right pointers in each node II

--- terry/leetcode/mytype/mytype.go
+++ terry/leetcode/mytype/mytype.go
@@ -1,7 +1,16 @@
 package mytype
 
+// TreeNode is a simple tree node struct.
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
 	Right *TreeNode
+}
+
+// Node is a simple tree node struct with next pointer.
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+	Next  *Node
 }
