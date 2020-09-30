2e366ed0b7f56503d2100cb79c557c847c0aca15
terry <mingyteng@163.com>
2020-09-30 12:47:42 +0800

    leetcode: insert into a binary search tree

---
+++ terry/leetcode/insertIntoABinarySearchTree.go
@@ -0,0 +1,24 @@
+package leetcode
+
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return &TreeNode{Val: val, Left: nil, Right: nil}
+	}
+	node := root
+	for {
+		if node.Val > val {
+			if node.Left == nil {
+				node.Left = &TreeNode{Val: val, Left: nil, Right: nil}
+				return root
+			}
+			node = node.Left
+		} else {
+			if node.Right == nil {
+				node.Right = &TreeNode{Val: val, Left: nil, Right: nil}
+				return root
+			}
+			node = node.Right
+		}
+	}
+
+}
