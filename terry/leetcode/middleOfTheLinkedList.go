4d2813946656786b1ed1fa6a16c589fb5b06ec64
terry <mingyteng@163.com>
2020-10-20 22:42:23 +0800

    leetcode: middle of the linked list

---
+++ terry/leetcode/middleOfTheLinkedList.go
@@ -0,0 +1,10 @@
+package leetcode
+
+func MiddleNode(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return slow
+}
