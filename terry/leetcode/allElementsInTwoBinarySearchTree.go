43e18065f9dcaf8dee68db525b938e16852d539d
terry <mingyteng@163.com>
2020-10-23 22:02:23 +0800

    leetcode: all elements in two binary search tree

--- terry/leetcode/allElementsInTwoBinarySearchTree.go
+++ terry/leetcode/allElementsInTwoBinarySearchTree.go
@@ -39,3 +39,53 @@ func mergeSorted(arr1 []int, arr2 []int) (res []int) {
 	}
 	return res
 }
+
+// GetAllElementsI 使用goroutine实现
+// 同时对两棵树进行中序排列，并将结果传递到两个channel中
+// 读取两个channel的数据
+func GetAllElementsI(root1 *TreeNode, root2 *TreeNode) []int {
+	ch1, ch2 := make(chan int), make(chan int)
+	out := make(chan int)
+	res := []int{}
+	if root1 != nil {
+		go treeValToChannel(root1, root1.Val, ch1)
+	} else {
+		close(ch1)
+	}
+	if root2 != nil {
+		go treeValToChannel(root2, root2.Val, ch2)
+	} else {
+		close(ch2)
+	}
+
+	go func() {
+		v1, ok1 := <-ch1
+		v2, ok2 := <-ch2
+		for ok1 || ok2 {
+			if ok1 && (v1 < v2 || !ok2) {
+				out <- v1
+				v1, ok1 = <-ch1
+			} else {
+				out <- v2
+				v2, ok2 = <-ch2
+			}
+		}
+		close(out)
+	}()
+	for val := range out {
+		res = append(res, val)
+	}
+	return res
+}
+
+func treeValToChannel(root *TreeNode, rootVal int, ch chan int) {
+	if root == nil {
+		return
+	}
+	treeValToChannel(root.Left, rootVal, ch)
+	ch <- root.Val
+	treeValToChannel(root.Right, rootVal, ch)
+	if rootVal == root.Val {
+		close(ch)
+	}
+}
