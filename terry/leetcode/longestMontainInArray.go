b9ba40551802e47734167cdda78f2b7668d8e340
terry <mingyteng@163.com>
2020-10-25 13:03:42 +0800

    leetcode: longest montain in array

--- terry/leetcode/longestMontainInArray.go
+++ terry/leetcode/longestMontainInArray.go
@@ -36,3 +36,39 @@ func LongestMountain(A []int) int {
 	}
 	return maxLength
 }
+
+// LongestMountain2 使用动态规划计算最长山脉的长度
+func LongestMountain2(A []int) int {
+	if len(A) < 3 {
+		return 0
+	}
+	// left[i], right[i] 分别代表以i为山顶到左山脚/右山脚的距离
+	// 如果 A[i] > A[i-1]，那么left[i] = left[i-1] + 1，否则left[i] = 0
+	// 同理 A[i] > A[i+1]，那么right[i] = right[i+1] + 1，否则right[i] = 0
+	// 只有当left[i]>0且right[i]>0时，i才可能是山顶
+	left, right := make([]int, len(A)), make([]int, len(A))
+
+	for i := 1; i < len(A); i++ {
+		if A[i] > A[i-1] {
+			left[i] = left[i-1] + 1
+		} else {
+			left[i] = 0
+		}
+	}
+	for i := len(A) - 2; i >= 0; i-- {
+		if A[i] > A[i+1] {
+			right[i] = right[i+1] + 1
+		} else {
+			right[i] = 0
+		}
+	}
+
+	maxLength := 0
+	for i := 0; i < len(A); i++ {
+		if left[i] == 0 || right[i] == 0 {
+			continue
+		}
+		maxLength = max(maxLength, left[i]+right[i]+1)
+	}
+	return maxLength
+}
