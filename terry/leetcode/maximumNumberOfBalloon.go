98874afc9dae7be602a18be62f054ba55d18a163
terry <mingyteng@163.com>
2020-10-28 20:25:32 +0800

    leetcode: maximum number of balloon

---
+++ terry/leetcode/maximumNumberOfBalloon.go
@@ -0,0 +1,23 @@
+package leetcode
+
+import "math"
+
+func MaxNumberOfBalloons(text string) int {
+	table := make(map[rune]int)
+	for _, ch := range text {
+		table[rune(ch)] = table[rune(ch)] + 1
+	}
+
+	return minList(table['b'], table['a'], table['l']/2, table['o']/2, table['n'])
+
+}
+
+func minList(a ...int) int {
+	minNum := math.MaxInt32
+	for _, val := range a {
+		if minNum > val {
+			minNum = val
+		}
+	}
+	return minNum
+}
