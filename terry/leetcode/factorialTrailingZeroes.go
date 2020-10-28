877e18a377a9ffd7014d5e5b7a10925880005cd7
terry <mingyteng@163.com>
2020-10-28 20:25:52 +0800

    leetcode: factoriaTrailingzeroes.go

---
+++ terry/leetcode/factorialTrailingZeroes.go
@@ -0,0 +1,25 @@
+package leetcode
+
+func TrailingZeroes(n int) int {
+	five, two := 0, 0
+	for i := 2; i <= n; i++ {
+		for tmp := i; tmp%2 == 0; tmp /= 5 {
+			two++
+		}
+		for tmp := i; tmp%5 == 0; tmp /= 5 {
+			five++
+		}
+	}
+	return min(two, five)
+}
+
+func TrailingZeroesI(n int) int {
+	five := 0
+	for i := 5; i <= n; i += 5 {
+		for tmp := i; tmp%5 == 0; tmp = tmp / 5 {
+			five++
+		}
+
+	}
+	return five
+}
