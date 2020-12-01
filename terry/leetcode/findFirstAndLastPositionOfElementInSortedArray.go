3286a2383d5f7162885d83c0c0c2f5b1775b18ae
terry <mingyteng@163.com>
2020-12-01 22:05:51 +0800

    leetcode: find first and last position of element in sorted array

---
+++ terry/leetcode/findFirstAndLastPositionOfElementInSortedArray.go
@@ -0,0 +1,35 @@
+package leetcode
+
+// 两次二分查找
+func searchRange(nums []int, target int) (ret []int) {
+	length := len(nums)
+	start, end := 0, length-1
+	for start < end {
+		mid := (start + end) / 2
+		if nums[mid] == target {
+			end = mid
+		} else if nums[mid] < target {
+			start = mid + 1
+		} else {
+			end = mid - 1
+		}
+	}
+	if nums[start] == target {
+		ret = append(ret, start)
+	} else {
+		return []int{-1, -1}
+	}
+	// 第二次可以缩小范围
+	start, end = ret[0], length-1
+	for start < end {
+		mid := (start + end + 1) / 2
+		if nums[mid] == target {
+			start = mid
+		} else if nums[mid] < target {
+			start = mid + 1
+		} else {
+			end = mid - 1
+		}
+	}
+	return append(ret, end)
+}
